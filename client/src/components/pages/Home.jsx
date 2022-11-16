bb1b66c645fe8aec0542c371196b8d0d95f9afe0
MindofSkk <helpmeskk@gmail.com>
2022-11-16 23:41:22 +0530

    work_in_progress

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,19 +1,53 @@
 import React from "react";
 import { Box, Button, Heading, Image } from "@chakra-ui/react";
-import styles from "./Home.module.css";
+import styles from "./css/Home.module.css";
 
 export const Home = () => {
+  const imagedata = [
+    "https://sunedison.in/wp-content/uploads/2022/01/Vector-1.png",
+    "https://sunedison.in/wp-content/uploads/2022/02/noun-solar-panel-4413713-1.png",
+    "https://sunedison.in/wp-content/uploads/2022/01/noun-solar-2990902-2.png",
+    "https://sunedison.in/wp-content/uploads/2022/01/Trees-saved.png",
+  ];
   return (
-    <div className={styles.firstdiv}>
-      <div className={styles.content}>
-      
-        <Heading size='lg' fontSize='80px'>Savings <br />
-         for a lifetime</Heading>
-        <Heading mb={4} fontWeight='100' fontSize='45px'>Get the SunEdison advantage</Heading>
+    <div>
+      <div className={styles.firstdiv}>
+        <div className={styles.content}>
+          <Heading size="lg" fontSize="80px">
+            Savings <br />
+            for a lifetime
+          </Heading>
+          <Heading mb={4} fontWeight="100" fontSize="45px">
+            Get the SunEdison advantage
+          </Heading>
 
-      
-        <button className={styles.bluebtn}>Compute your solar savings</button>
-        <button  className={styles.orgbtn}>Explore solar solutions</button>
+          <button className={styles.bluebtn}>Compute your solar savings</button>
+          <button className={styles.orgbtn}>Explore solar solutions</button>
+        </div>
+      </div>
+
+      <div className={styles.secdiv}>
+        <div>
+          {" "}
+          <img src={imagedata[0]} alt="" /> 200+ <br />
+          <h1>Cities in India</h1>
+        </div>
+        <div>
+          {" "}
+          <img src={imagedata[1]} alt="" />
+          25000+ <br />
+          <h1>INSTALLATIONS</h1>
+        </div>
+        <div>
+          <img src={imagedata[2]} alt="" />
+          100MWp+ <br />
+          <h1>CAPACITY INSTALLED</h1>
+        </div>
+        <div>
+          <img src={imagedata[3]} alt="" />
+          10M+ <br />
+          <h1>TREES SAVED</h1>
+        </div>
       </div>
     </div>
   );
--- client/src/components/pages/Solar_Calculator.jsx
+++ client/src/components/pages/Solar_Calculator.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+import styles from "./css/Solar_Calculator.module.css";
 
 export const Solar_Calculator = () => {
   return (
-    <div>Solar_Calculator</div>
-  )
-}
+    <div className={styles.box}>
+      Home » Solar Calculator
+      <h1></h1>
+      <div className={styles.firstBox}>
+        {" "}
+        <h2 className={styles.s2}>
+          | Find the right rooftop solar system and save
+        </h2>
+        <h1 className={styles.s1}>SunEdison Solar Calculator </h1>
+        <p>
+          SunEdison’s solar calculator helps you easily understand the benefit
+          of using solar products. Use this simple tool to gauge potential
+          savings with <br /> our rooftop solar system for your home or
+          commercial property. It will also tell you the best rooftop solar
+          product for your premises.
+        </p>
+      </div>
+    </div>
+  );
+};
